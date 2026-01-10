aa572de16296e8e88591122493e6b3c3cc52ec5d
Molly Wu <mollyw236@gmail.com>
2026-01-10 14:06:36 -0500

    Return allow and deny decisions with timing

--- memory.go
+++ memory.go
@@ -48,20 +48,40 @@ func (m *MemoryLimiter) Allow(ctx context.Context, id Identity, limit Limit) (De
 			RetryAfter: 0,
 			ResetTime:  now,
 		}, nil
+	} else {
+		elapsed := now.Sub(st.lastRefill)
+		if elapsed < 0 {
+			elapsed = 0
+		}
+		delta := float64(elapsed) / float64(limit.Period)
+		tokensToAdd := delta * float64(limit.Rate)
+		newBalance := st.tokens + tokensToAdd
+		if newBalance > float64(limit.Burst) {
+			newBalance = float64(limit.Burst)
+		}
+		st.tokens = newBalance
+
+		st.lastRefill = now
+
+		if st.tokens >= 1 {
+			st.tokens -= 1
+			return Decision{
+				Allow:      true,
+				Remaining:  int64(st.tokens),
+				RetryAfter: 0,
+				ResetTime:  now,
+			}, nil
+		} else {
+			costPerToken := float64(limit.Period) / float64(limit.Rate)
+			missing := 1.0 - st.tokens
+			waitParams := missing * costPerToken
+			return Decision{
+				Allow:      false,
+				Remaining:  int64(st.tokens),
+				RetryAfter: time.Duration(waitParams),
+				ResetTime:  now.Add(time.Duration(waitParams)),
+			}, nil
+		}
 	}
 
-	elapsed := now.Sub(st.lastRefill)
-	if elapsed < 0 {
-		elapsed = 0
-	}
-	delta := float64(elapsed) / float64(limit.Period)
-	tokensToAdd := delta * float64(limit.Rate)
-	newBalance := st.tokens + tokensToAdd
-	if newBalance > float64(limit.Burst) {
-		newBalance = float64(limit.Burst)
-	}
-	st.tokens = newBalance
-	st.lastRefill = now
-	// TODO: allow/deny check
-	return Decision{Allow: true, Remaining: int64(st.tokens), ResetTime: now}, nil
 }

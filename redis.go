8d3a63e3d0c9c069d007ec04759139652c0f3090
Molly Wu <mollyw236@gmail.com>
2026-01-17 16:06:49 -0500

    Add WithTimeout and WithRecorder options

--- redis.go
+++ redis.go
@@ -14,7 +14,15 @@ type Option func(*limiterConfig)
 
 // WithPrefix sets the Redis key prefix. Default is "limiter:".
 func WithPrefix(prefix string) Option {
-	return func(c *limiterConfig) {
-		c.prefix = prefix
-	}
+	return func(c *limiterConfig) { c.prefix = prefix }
+}
+
+// WithTimeout sets the timeout for Redis operations during initialization. Default is 5s.
+func WithTimeout(timeout time.Duration) Option {
+	return func(c *limiterConfig) { c.timeout = timeout }
+}
+
+// WithRecorder sets the metrics recorder. Default is NoOpMetricsRecorder.
+func WithRecorder(recorder MetricsRecorder) Option {
+	return func(c *limiterConfig) { c.recorder = recorder }
 }

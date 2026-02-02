f30561b826e62acc2bd6a375d9f237379362c86c
Molly Wu <mollyw236@gmail.com>
2026-02-02 14:41:24 -0500

    Count requests currently within the window

--- sliding_window.lua
+++ sliding_window.lua
@@ -8,5 +8,20 @@ local nonce  = ARGV[5]
 local window_start = now - window
 redis.call('ZREMRANGEBYSCORE', key, '-inf', window_start)
 
--- TODO: count and decide
-return {0, 0, "0", "0"}
+local count = redis.call('ZCARD', key)
+
+local allowed     = 0
+local remaining   = 0
+local retry_after = 0.0
+local reset_time  = 0.0
+
+if count + cost <= limit then
+	-- TODO: record and return
+	allowed   = 1
+	remaining = limit - count - cost
+	reset_time = now
+else
+	allowed = 0
+end
+
+return {allowed, remaining, tostring(retry_after), tostring(reset_time)}

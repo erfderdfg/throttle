8593321fb3004c55cc6dc9f32b8db53f03306418
Molly Wu <mollyw236@gmail.com>
2026-02-10 14:23:02 -0500

    Initialize token bucket limiter for /api/data

--- cmd/example-server/main.go
+++ cmd/example-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	limiter "github.com/erfderdfg/go-rate-limiter"
 	"github.com/prometheus/client_golang/prometheus"
@@ -18,7 +19,16 @@ func main() {
 
 	reg := prometheus.NewRegistry()
 	rec := limiter.NewPrometheusRecorder(reg)
-	_ = rec
+
+	tbLimiter, err := limiter.NewRedisLimiter(client,
+		limiter.WithPrefix("demo:tb:"),
+		limiter.WithTimeout(100*time.Millisecond),
+		limiter.WithRecorder(rec),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = tbLimiter
 
 	log.Printf("Server starting (Redis: %s)", redisAddr)
 }
